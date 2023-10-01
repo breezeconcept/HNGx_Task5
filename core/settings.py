a2db8e919571cebe39a67216637985c19cc5f52a
Breeze Concept <breezeconcept3@gmail.com>
2023-10-01 21:15:01 +0100

    updated settings

--- core/settings.py
+++ core/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-0(o-d-k8ckv*x=#hr&5ll(_j_*!ikds#qndr5xa*^gk49hc#yi
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
