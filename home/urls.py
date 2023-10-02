3542b5c835c2cd6226c9e2197b734bb4520debdf
Breeze Concept <breezeconcept3@gmail.com>
2023-10-02 13:43:58 +0100

    updated

--- home/urls.py
+++ home/urls.py
@@ -2,22 +2,23 @@ from django.urls import path, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 
-from rest_framework import permissions
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
 
-schema_view = get_schema_view(
-    openapi.Info(
-        title="Your API Title",
-        default_version='v1',
-        description="Your API Description",
-        terms_of_service="https://www.example.com/terms/",
-        contact=openapi.Contact(email="contact@example.com"),
-        license=openapi.License(name="Your License"),
-    ),
-    public=True,
-    permission_classes=(permissions.AllowAny,),
-)
+# from rest_framework import permissions
+# from drf_yasg.views import get_schema_view
+# from drf_yasg import openapi
+
+# schema_view = get_schema_view(
+#     openapi.Info(
+#         title="Your API Title",
+#         default_version='v1',
+#         description="Your API Description",
+#         terms_of_service="https://www.example.com/terms/",
+#         contact=openapi.Contact(email="contact@example.com"),
+#         license=openapi.License(name="Your License"),
+#     ),
+#     public=True,
+#     permission_classes=(permissions.AllowAny,),
+# )
 
 from .views import (
     start_recording,
@@ -27,9 +28,9 @@ from .views import (
 )
 
 urlpatterns = [
-    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    # re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    # path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    # path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('start-recording/', start_recording, name='start-recording'),
     path('receive-video-chunk/', receive_video_chunk, name='receive-video-chunk'),
     path('finalize-recording/', finalize_recording, name='finalize-recording'),
